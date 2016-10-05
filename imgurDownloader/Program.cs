352a3acf1fb3d894433f7cb2f44f8fb4880d59e9
producer-zombie <sascha@producerzombie.com>
2016-10-06 00:41:43 +0200

    Initial commit with basic C# commandline shell.

---
+++ imgurDownloader/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace imgurDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+    }
+}

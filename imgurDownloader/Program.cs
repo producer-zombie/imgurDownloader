2fd827535ad2c9240403616806cb85c262099779
producer-zombie <sascha@producerzombie.com>
2016-10-06 23:23:03 +0200

    Got very basic download working with WebClient (yay, made it work). Next step will be separating the WebClient download into a separate method that can be called from Main().

--- imgurDownloader/Program.cs
+++ imgurDownloader/Program.cs
@@ -4,13 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
+using System.Net;
 
 namespace imgurDownloader
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+
+            string fileNameLocation = "url_file_name";
+
+            //   string saveLocationName = "file_location_path";
+
+            Console.WriteLine("Enter domain without http/https of the image you want to download.");
+
+            fileNameLocation = Console.ReadLine();
+
+            //  Console.WriteLine("Enter the save location of the file.");
+
+            //  saveLocationName = Console.ReadLine();
+
+            WebClient webClient = new WebClient();
+            webClient.DownloadFile(fileNameLocation, @"C:\Pictures\filename.jpg");
+
+            Console.ReadLine();
         }
 
     }
